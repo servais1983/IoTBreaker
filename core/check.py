7a531ee6749e224929b25ecd512b842bfe965759
CyberS <stservais0409@gmail.com>
2025-05-21 20:49:37 +0200

    Ajout du module de vérification

---
+++ core/check.py
@@ -0,0 +1,4 @@
+def run(ip):
+    print(f"[*] Vérification de vulnérabilités connues sur {ip}...")
+    print("[+] (Démo) Test de connexion Telnet sans mot de passe...")
+    print("[+] (Démo) Scan MQTT sans authentification (à intégrer avec paho-mqtt)...")
